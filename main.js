c53697c8be2aa044c8f82753b2639f373d5c4063
Napermial <h>
2019-09-15 21:13:54 +0200

    callback

--- main.js
+++ main.js
@@ -6,17 +6,24 @@ let config = {
     }
 };
 
-let commands;
-
 axios.get('https://registration.hungary.chaosstack.com/api/qualify/user_commands', config)
-    .then(response => (console.log(response.data.userCommands = commands)));
+    .then(response => (compareValues(response.data.userCommands)));
 
+function compareValues(commands) {
+    let mapping = require('./mapping.json');
 
-let mapping = require('./mapping.json');
-
-for (command in commands) {
-
+    for (let command in commands) {
+        for (let map in mapping) {
+            if (command.user === map.user) {
+                for (room in map.rooms) {
+                    if (room === command.targetRoom) {
+                        let lights = room.values;
+                        let data = command.action;
+                        data.add(lights);
+                        axios.post('https://registration.hungary.chaosstack.com/api/qualify/set_lights', data, config);
+                    }
+                }
+            }
+        }
+    }
 }
-//axios.post('https://registration.hungary.chaosstack.com/api/qualify/set_lights', , config);
-
-
